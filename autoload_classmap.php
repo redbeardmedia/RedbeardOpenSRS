1d5be9b62b9ddb30f487199fce925fe9abdf378d
Redbeard Media <info@redbeardmedia.nl>
2013-02-15 10:52:58 +0100

    Changed namespace to RedbeardOpenSRS

--- autoload_classmap.php
+++ autoload_classmap.php
@@ -1,5 +1,5 @@
 <?php
 return array (
-	'RbmOpensrs\\Domain' 	=> __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'RbmOpensrs' . DIRECTORY_SEPARATOR . 'Domain.php',
-	'RbmOpensrs\\Test' 		=> __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'RbmOpensrs' . DIRECTORY_SEPARATOR . 'Test.php',
+	'RedbeardOpenSRS\\Domain' 	=> __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'RedbeardOpenSRS' . DIRECTORY_SEPARATOR . 'Domain.php',
+	'RedbeardOpenSRS\\Test' 		=> __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'RedbeardOpenSRS' . DIRECTORY_SEPARATOR . 'Test.php',
 );
--- src/RedbeardOpenSRS/Domain.php
+++ src/RedbeardOpenSRS/Domain.php
@@ -1,5 +1,5 @@
 <?php
-namespace RbmOpensrs;
+namespace RedbeardOpenSRS;
 
 class Domain
 {
